55ed4a2039542a3b41a9f0e63f4f1d035c34efb4
Bryan Giger <bryan@gigerbyt.es>
2020-10-11 06:06:52 +0200

    add dynamic charts

--- src/frontend/components/organizations/Dashboard.tsx
+++ src/frontend/components/organizations/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import { ResponsiveContainer, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
+import { ResponsiveContainer, BarChart, Bar, Legend, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 
 import blue from '@material-ui/core/colors/blue'
@@ -9,6 +9,12 @@ import red from '@material-ui/core/colors/red'
 import CardContainer from 'frontend/components/_common/CardContainer'
 
 import Grid from '@material-ui/core/Grid'
+
+const getRandomInt = (min: number, max: number): number => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -29,16 +35,19 @@ const Dashboard = (): ReactElement => {
 
   const data = [
     {
-      name: 'Oct 7', uv: 4000, pv: 2400, amt: 2400
+      name: 'Oct 7', negative: getRandomInt(1000,5000), positive: getRandomInt(100,500)
     },
     {
-      name: 'Oct 8', uv: 3000, pv: 1398, amt: 2210
+      name: 'Oct 8', negative: getRandomInt(1000,5000), positive: getRandomInt(100,500)
     },
     {
-      name: 'Oct 9', uv: 2000, pv: 9800, amt: 2290
+      name: 'Oct 9', negative: getRandomInt(1000,5000), positive: getRandomInt(100,500)
     },
     {
-      name: 'Oct 10', uv: 2780, pv: 3908, amt: 2000
+      name: 'Oct 10', negative: getRandomInt(1000,5000), positive: getRandomInt(100,500)
+    },
+    {
+      name: 'Oct 12', negative: getRandomInt(1000,5000), positive: getRandomInt(100,500)
     }
   ]
 
@@ -49,20 +58,65 @@ const Dashboard = (): ReactElement => {
           container spacing={3}
           justify="center"
           alignItems="center">
-        <Grid item xs={10}>
+        <Grid item xs={5}>
         <CardContainer
           title = {'Registrations per day'}
           content = {
             <>
-              <ResponsiveContainer width={'100%'} height={500}>
+              <ResponsiveContainer width={'100%'} height={300}>
 
                 <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                  <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                  <Line type="monotone" dataKey="negative" stroke="#8884d8" />
                   <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                   <XAxis dataKey="name" />
                   <YAxis />
                   <Tooltip />
                 </LineChart>
+              </ResponsiveContainer>
+            </>
+          }
+        />
+        </Grid>
+        <Grid item xs={5}>
+        <CardContainer
+          title = {'People Tested per day'}
+          content = {
+            <>
+              <ResponsiveContainer width={'100%'} height={300}>
+
+                <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                  <Line type="monotone" dataKey="positive" stroke="#8884d8" />
+                  <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Tooltip />
+                </LineChart>
+              </ResponsiveContainer>
+            </>
+          }
+        />
+        </Grid>
+        <Grid item xs={10}>
+        <CardContainer
+          title = {'Result Distribution'}
+          content = {
+            <>
+              <ResponsiveContainer width={'100%'} height={500}>
+
+              <BarChart
+                data={data}
+                margin={{
+                  top: 20, right: 50, left: 50, bottom: 5
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 2" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="negative" stackId="a" fill="#8884d8" />
+                <Bar dataKey="positive" stackId="a" fill="#82ca9d" />
+              </BarChart>
               </ResponsiveContainer>
             </>
           }
