8d06b80df66f3f0bf943f296e82aeb9cbbeaa7f1
Bryan Giger <bryan@gigerbyt.es>
2020-10-11 04:38:11 +0200

    set text for book appointment

--- src/frontend/components/labs/LabList/index.tsx
+++ src/frontend/components/labs/LabList/index.tsx
@@ -1,7 +1,6 @@
 import { Typography } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import CardContainer from 'frontend/components/_common/CardContainer'
-import Grid from '@material-ui/core/Grid'
 import React, { ReactElement } from 'react'
 const LabList = (): ReactElement => {
 
@@ -10,10 +9,19 @@ const LabList = (): ReactElement => {
         <CardContainer
           title='Laboratory 1'
           content={
-            <Typography>Test here. It is 3 miles from the event location.</Typography>
+            <>
+              <Typography>Test here. It is 3 miles from the event location.</Typography>
+            </>
           }
           actions={
-            <Button color="primary">Book an Appointment</Button>
+            <Button
+              color="primary"
+              onClick={(): void =>{
+                console.log('ive been clicked')
+              }}
+            >
+                Book an Appointment
+            </Button>
           }
         />
       </>
