f3939ba57f8a9f5aeb34b8572e00b425817227c6
migsgemotra <migsgemotra@gmail.com>
2020-10-11 11:24:37 +0800

    Added mutation for book

--- src/frontend/components/labs/LabList/index.tsx
+++ src/frontend/components/labs/LabList/index.tsx
@@ -1,8 +1,11 @@
 import { Typography } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import CardContainer from 'frontend/components/_common/CardContainer'
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
+import TextField from '@material-ui/core/TextField'
 const LabList = (): ReactElement => {
+
+  const [email, setEmail] = useState<string>('')
 
     return (
       <>
@@ -11,6 +14,19 @@ const LabList = (): ReactElement => {
           content={
             <>
               <Typography>Test here. It is 3 miles from the event location.</Typography>
+              <p/>
+              <Typography>Input your email to confirm the booking and receive the next steps to take.</Typography>
+              <TextField
+                size={'small'}
+                variant={'outlined'}
+                fullWidth
+                margin={'dense'}
+                label={'Email'}
+                value={email}
+                onChange={(e): void => {
+                  setEmail(e.target.value)
+                }}
+              />
             </>
           }
           actions={
